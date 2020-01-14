8d631d73b8a5ca4923ec73c3af762b8c58dc3909
Jin <jijung@chapman.edu>
2020-01-14 12:48:47 -0500

    initial commit

---
+++ web-s.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+#from BeautifulSoup import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+ratings=[] #List to store rating of the product
+driver.get("https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniqBStoreParam1=val1&wid=11.productCard.PMU_V2")
+
